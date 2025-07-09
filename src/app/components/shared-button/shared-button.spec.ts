f15e38a67a213945d92d7cd44955ff1d7a2f3652
HemanthSaiTeja <hemanthsaiteja881@gmail.com>
2025-07-09 18:44:22 +0530

    added sharedButton

---
+++ src/app/components/shared-button/shared-button.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SharedButton } from './shared-button';
+
+describe('SharedButton', () => {
+  let component: SharedButton;
+  let fixture: ComponentFixture<SharedButton>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SharedButton]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SharedButton);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/shared-button/shared-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-button',
+  imports: [],
+  templateUrl: './shared-button.html',
+  styleUrl: './shared-button.scss'
+})
+export class SharedButton {
+
+}
